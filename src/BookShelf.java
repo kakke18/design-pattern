ac5c937696788aaa0c77ee259dc999b175f7a099
kakke18 <tanaka-kakeru-xm@ynu.jp>
2019-04-08 23:43:47 +0900

    add: practice

--- src/BookShelf.java
+++ src/BookShelf.java
@@ -1,19 +1,39 @@
-public class BookShelf implements Aggregate {
-    private Book[] books;
-    private int last;
+import java.util.ArrayList;
 
-    public BookShelf(int maxsize) {
-        this.books = new Book[maxsize];
+public class BookShelf implements Aggregate {
+//    private Book[] books;
+//    private int last;
+//
+//    public BookShelf(int maxsize) {
+//        this.books = new Book[maxsize];
+//    }
+//    public Book getBookAt(int index) {
+//        return books[index];
+//    }
+//    public void appendBook(Book book) {
+//        this.books[last] = book;
+//        last++;
+//    }
+//    public int getLength() {
+//        return last;
+//    }
+//    public Iterator iterator() {
+//        return new BookShelfIterator(this);
+//    }
+
+    private ArrayList books;
+
+    public BookShelf(int initialize) {
+        this.books = new ArrayList(initialize);
     }
     public Book getBookAt(int index) {
-        return books[index];
+        return (Book)books.get(index);
     }
     public void appendBook(Book book) {
-        this.books[last] = book;
-        last++;
+        books.add(book);
     }
     public int getLength() {
-        return last;
+        return books.size();
     }
     public Iterator iterator() {
         return new BookShelfIterator(this);
