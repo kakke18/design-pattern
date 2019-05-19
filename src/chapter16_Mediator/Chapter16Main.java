e5e4f5269a782930182c60b9b2134977e2d3385b
kakke18 <tanaka-kakeru-xm@ynu.jp>
2019-05-20 00:27:11 +0900

    fix: rename main class

--- src/chapter16_Mediator/Chapter16Main.java
+++ src/chapter16_Mediator/Chapter16Main.java
@@ -1,6 +1,6 @@
 package chapter16_Mediator;
 
-public class Chapter15Main {
+public class Chapter16Main {
     public static void main(String[] args) {
         new LoginFrame("Mediator Sample");
     }
