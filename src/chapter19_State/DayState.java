f02e320e801f5b1dcf30caf382cb9973e5ad44cc
kakke18 <tanaka-kakeru-xm@ynu.jp>
2019-05-30 15:45:12 +0900

    add: chapter19

---
+++ src/chapter19_State/DayState.java
@@ -0,0 +1,44 @@
+package chapter19_State;
+
+import chapter08_AbstractFactory.factory.Page;
+
+public class DayState implements State {
+    private static DayState siglton = new DayState();
+
+    private DayState() {
+    }
+
+    public static State getInstance() {
+        return siglton;
+    }
+
+    @Override
+    public void doClock(Context context, int hour) {
+        if (hour < 9 || 17 <= hour) {
+            context.changeState(NightState.getInstance());
+        } else if (12 <= hour && hour < 13) {
+            context.changeState(NoonState.getInstance());
+        }
+    }
+
+    @Override
+    public void doUse(Context context) {
+        context.recordLog("金庫使用（昼間)");
+    }
+
+    @Override
+    public void doAlarm(Context context) {
+        context.callSecurityCenter("非常ベル（昼間）");
+        context.changeState(UrgentState.getInstance());
+    }
+
+    @Override
+    public void doPhone(Context context) {
+        context.callSecurityCenter("通常の通話（昼間）");
+    }
+
+    @Override
+    public String toString() {
+        return "[昼間]";
+    }
+}
---
+++ src/chapter19_State/UrgentState.java
@@ -0,0 +1,36 @@
+package chapter19_State;
+
+public class UrgentState implements State {
+    private static UrgentState singleton = new UrgentState();
+
+    public UrgentState() {
+    }
+
+    public static State getInstance() {
+        return singleton;
+    }
+
+    @Override
+    public void doClock(Context context, int hour) {
+    }
+
+    @Override
+    public void doUse(Context context) {
+        context.callSecurityCenter("非常：非常時の金庫使用");
+    }
+
+    @Override
+    public void doAlarm(Context context) {
+        context.callSecurityCenter("非常ベル（非常時）");
+    }
+
+    @Override
+    public void doPhone(Context context) {
+        context.callSecurityCenter("通常の通話（非常時）");
+    }
+
+    @Override
+    public String toString() {
+        return "[非常時]";
+    }
+}
