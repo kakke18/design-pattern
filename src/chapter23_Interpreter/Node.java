c2d14feba9781b3774f0c1d8cb4991c8cc971077
kakke18 <tanaka-kakeru-xm@ynu.jp>
2019-06-08 00:56:56 +0900

    add: chapter23

---
+++ src/chapter23_Interpreter/Node.java
@@ -0,0 +1,5 @@
+package chapter23_Interpreter;
+
+public abstract class Node {
+    public abstract void parse(Context context) throws ParseException;
+}
---
+++ src/chapter23_Interpreter/ParseException.java
@@ -0,0 +1,7 @@
+package chapter23_Interpreter;
+
+public class ParseException extends Exception {
+    public ParseException(String msg) {
+        super(msg);
+    }
+}
